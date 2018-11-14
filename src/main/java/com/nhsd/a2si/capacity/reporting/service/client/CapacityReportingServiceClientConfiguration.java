58d77f4ed5c9a0cc0ef00c575ec3f164873ba748
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-11-14 11:06:51 +0000

    Creates the project for holding the client's code

---
+++ src/main/java/com/nhsd/a2si/capacity/reporting/service/client/CapacityReportingServiceClientConfiguration.java
@@ -0,0 +1,21 @@
+package com.nhsd.a2si.capacity.reporting.service.client;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class CapacityReportingServiceClientConfiguration {
+
+    // The Rest Template is configured to use the components defined in the http client, which include
+    // the pooling connection manager and the request configuration
+    @Bean
+    public RestTemplate restTemplate(CloseableHttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+
+}
---
+++ src/test/java/com/nhsd/a2si/capacity/reporting/service/client/CapacityReportingServiceClientConfigurationTest.java
@@ -0,0 +1,7 @@
+package com.nhsd.a2si.capacity.reporting.service.client;
+
+import static org.junit.Assert.*;
+
+public class CapacityReportingServiceClientConfigurationTest {
+
+}
